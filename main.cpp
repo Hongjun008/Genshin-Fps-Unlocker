f468c625b11b510f84b4559d2845810a97721939
Hongjun008 <hongjun-ms@outlook.com>
2024-10-28 21:50:04 +0800

    add file info etc. pre-release(beta)version

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
-    MainWindow window("Genshin Impact FPS Unlocker");
+    const char appName[]{"Genshin Impact FPS Unlocker"};
+    QApplication::setApplicationName(appName);
+    QApplication::setApplicationVersion("0.0.1");
+    QApplication::setOrganizationName("Hongjun008");
+    QApplication::setOrganizationDomain("hongjun.tech");
+    MainWindow window(appName);
     window.show();
 
     return QApplication::exec();
